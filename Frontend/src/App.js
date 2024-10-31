358462a33bed208edbc72e8b6f21a95c12c2980d
shobana-ravi <shobana.ravi.a@gmail.com>
2024-10-30 22:56:52 -0500

    Navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const tasks = [
+  {
+    id: 1,
+    title: "Wash the Dishes",
+    description: "Make sure all dishes are washed and dried",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Walk the Dog",
+    description: "Take the dog around the nieghborhood",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Help Mom",
+    description: "Help mom clean the house",
+    completed: true
+  },
+
+
+]
+
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+        viewCompleted: false,
+        taskList: tasks,
+    };
+  }
+
+  displayCompleted = status => {
+    if (status){
+      return this.setstatus({ viewCompleted: true });
+    }
+    return this.setstatus({ viewCompleted: false });
+  }
+
+  renderTabList = () => {
+    return (
+      < div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Completed
+            </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Incompleted
+            </span>
+      </div>
+    )
+  }
+
+  //Rendering items as incompleted || completed
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed === viewCompleted
+    );
+  };
+
+  //Rendering on the screen itself
+  render() {
+    return (
+      <div className = "App">
+        Hello World
+      </div>
+    )
+  }
+
 }
 
 export default App;
