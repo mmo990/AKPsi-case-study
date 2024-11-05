b7d5bc66b5ca2d7ce17972d90231ec6242b60548
pravan <pravangorthy21@gmail.com>
2024-11-04 23:20:33 -0600

    updated login screen

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -17,7 +17,7 @@ function Login({ onLogin }) {
 
   return (
     <div className="login-container">
-      <h2>Login</h2>
+      <h2 className="login-title">Login</h2>
       <form onSubmit={handleSubmit} className="login-form">
         <input
           type="text"
@@ -25,6 +25,7 @@ function Login({ onLogin }) {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
+          className="login-input"
         />
         <input
           type="password"
@@ -32,6 +33,7 @@ function Login({ onLogin }) {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          className="login-input"
         />
         <button type="submit" className="login-button">Login</button>
       </form>
